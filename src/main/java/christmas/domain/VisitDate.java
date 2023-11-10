c4ebbbf0e741922d22885fe0ddeff31f3d204316
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-10 19:55:55 +0900

    feat(VisitDate): 방문 날짜 validate 기능 구현
    - 방문 날짜가 해당 월의 날짜 범위 안에 없다면 예외 발생

--- src/main/java/christmas/domain/VisitDate.java
+++ src/main/java/christmas/domain/VisitDate.java
@@ -7,7 +7,18 @@ public class VisitDate {
         this.visitDay = visitDay;
     }
 
-    public static VisitDate of(final int visitDay) {
+    public static VisitDate of(final int visitDay, final int firstDay, final int endDay) {
+        validate(visitDay, firstDay, endDay);
         return new VisitDate(visitDay);
+    }
+
+    public static void validate(final int visitDay, final int firstDay, final int endDay) {
+        validateDayInMonth(visitDay, firstDay, endDay);
+    }
+
+    public static void validateDayInMonth(final int visitDay, final int firstDay, final int endDay) {
+        if (visitDay < firstDay || visitDay > endDay) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/christmas/domain/VisitDateTest.java
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -1,17 +1,31 @@
 package christmas.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class VisitDateTest {
+    private final int year = 2023;
+    private final Month month = Month.of(12);
+    private final int firstDay = 1;
+    private int endDay;
+
+    @BeforeEach
+    void setUp() {
+        LocalDate localDate = LocalDate.of(year, month, 1);
+        this.endDay = localDate.lengthOfMonth();
+    }
 
     @ParameterizedTest
     @DisplayName("입력한 날짜가 해당 월의 날짜 범위 안에 없다면 예외 발생")
     @ValueSource(ints = {-1, -2, 0, 32, 33, 34})
     void outOfRangeVisitDay(int visitDay) {
-        assertThatIllegalArgumentException().isThrownBy(() -> VisitDate.of(visitDay));
+        assertThatIllegalArgumentException().isThrownBy(() -> VisitDate.of(visitDay, this.firstDay, this.endDay));
     }
 }
