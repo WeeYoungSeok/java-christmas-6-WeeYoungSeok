0b5bdc811f10c9ce40b2829be865e68e6186913b
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-11 00:17:02 +0900

    refactor(EventCalendar): 변수명 수정

--- src/main/java/christmas/domain/EventCalendar.java
+++ src/main/java/christmas/domain/EventCalendar.java
@@ -4,14 +4,14 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public class EventCalendar {
-    private final LocalDate yearMonth;
+    private final LocalDate currentDate;
 
     public EventCalendar(int year, int month) {
-        this.yearMonth = LocalDate.of(year, month, 1);
+        this.currentDate = LocalDate.of(year, month, 1);
     }
 
     public boolean isWeekend(int day) {
-        DayOfWeek dayOfWeek = yearMonth.withDayOfMonth(day).getDayOfWeek();
+        DayOfWeek dayOfWeek = currentDate.withDayOfMonth(day).getDayOfWeek();
         return dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY;
     }
 }
