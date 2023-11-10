9561cebb004edec648e8c2806582508637c07ab1
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-10 20:09:40 +0900

    feat(ErrorMessage): 에러 메세지 추가

---
+++ src/main/java/christmas/message/ErrorMessage.java
@@ -0,0 +1,18 @@
+package christmas.message;
+
+public enum ErrorMessage {
+    PREFIX("[ERROR] "),
+    SUFFIX(" 다시 입력해 주세요."),
+    INVALID_DATE("유효하지 않은 날짜입니다."),
+    INVALID_ORDER("유효하지 안는 주문입니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
