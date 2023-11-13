212d7999ca165798c94288d888d890fc19c10491
WeeYoungSeok <dndrldzk@gmail.com>
2023-11-13 14:50:48 +0900

    refactor(EventDiscount): enum field refactoring

--- src/main/java/christmas/domain/contants/EventDiscount.java
+++ src/main/java/christmas/domain/contants/EventDiscount.java
@@ -1,15 +1,21 @@
 package christmas.domain.contants;
 
 public enum EventDiscount {
-    WEEKDAY(2023),
-    WEEKEND(2023),
-    SPECIAL(1000),
-    CHRISTMAS(1000);
+    WEEKDAY("평일 할인", 2023),
+    WEEKEND("주말 할인", 2023),
+    SPECIAL("특별 할인", 1000),
+    CHRISTMAS("크리스마스 디데이 할인", 1000);
 
+    private final String discountName;
     private final int discount;
 
-    EventDiscount(int discount) {
+    EventDiscount(String discountName, int discount) {
+        this.discountName = discountName;
         this.discount = discount;
+    }
+
+    public String getDiscountName() {
+        return discountName;
     }
 
     public int getDiscount() {
